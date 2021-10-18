7ad761ac5f39c720eb3ac31dcf0f17fc483a34df
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2021-10-18 11:45:49 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import MainPage from './pages/MainPage'
 import '../node_modules/slick-carousel/slick/slick.css'
 import '../node_modules/slick-carousel/slick/slick-theme.css'
+import { Router, Switch, Route } from 'react-router';
+import Themes from './pages/Themes';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <MainPage/>
-    
+    <BrowserRouter>
+
+
+      <Switch>
+        <Route exact path="/">
+          <MainPage />
+        </Route>
+        <Route path="/themes">
+          <Themes />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
+
   );
 }
 
